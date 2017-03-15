e7497035fd04560072e0113ba2693c168caa3953
vitoumh <vitoumh@hotmail.com>
2017-03-15 19:50:16 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* Javascript */
 $('.editor').trumbowyg({
-    svgPath: '/nombreRepo/icons.svg'
+    svgPath: '/vitoumh/lib_trumbowyg2/iconos/icons.svg'
     // svgPath: '/icons/icons.svg', // path of icons.svg
     // Habitualmente, se indica la ruta del SVG en este parámetro, pero sólo
     // en codepen, simplemente pegaremos el contenido del SVG en la parte
