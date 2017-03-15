1e460f0dcd82b1956d70b1d00bcfbcb0c5ac891d
vitoumh <vitoumh@hotmail.com>
2017-03-15 19:56:51 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,7 @@
 $( document ).ready(function() {
 
     $('.editor').trumbowyg({
-        svgPath: '/vitoumh/lib_trumbowyg2/iconos/icons.svg'
-        // svgPath: '/icons/icons.svg', // path of icons.svg
-        // Habitualmente, se indica la ruta del SVG en este parámetro, pero sólo
-        // en codepen, simplemente pegaremos el contenido del SVG en la parte
-        // del código HTML
+        svgPath: '/lib_trumbowyg2/iconos/icons.svg'
     });   
     
 });
