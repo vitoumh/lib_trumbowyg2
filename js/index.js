89a484c7a131771be13409e98591ad9fd99e248a
vitoumh <vitoumh@hotmail.com>
2017-03-15 19:55:21 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 /* Javascript */
-$('.editor').trumbowyg({
-    svgPath: '/vitoumh/lib_trumbowyg2/iconos/icons.svg'
-    // svgPath: '/icons/icons.svg', // path of icons.svg
-    // Habitualmente, se indica la ruta del SVG en este parámetro, pero sólo
-    // en codepen, simplemente pegaremos el contenido del SVG en la parte
-    // del código HTML
+$( document ).ready(function() {
+
+    $('.editor').trumbowyg({
+        svgPath: '/vitoumh/lib_trumbowyg2/iconos/icons.svg'
+        // svgPath: '/icons/icons.svg', // path of icons.svg
+        // Habitualmente, se indica la ruta del SVG en este parámetro, pero sólo
+        // en codepen, simplemente pegaremos el contenido del SVG en la parte
+        // del código HTML
+    });   
+    
 });
